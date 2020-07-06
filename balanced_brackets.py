79edd55ec5c345d39de97d996c5326e111368218
Vincent Sanders <vincesanders@hotmail.com>
2020-07-06 15:40:40 -0500

    Added comments to balanced brackets code.

--- balanced_brackets.py
+++ balanced_brackets.py
@@ -1,27 +1,52 @@
 def balancedBrackets(string):
     # Write your code here
+
+    # This dictionary's purpose is to make it easier to check for a matching bracket.
     bracket_dict = {
         '[': ']',
         '(': ')',
         '{': '}',
         '|': '|'
     }
+
+    # This set is used to isolated the characters of the string we're interested in.
+    # if the char isn't in this set, we don't care about it.
     possible_matches = {'[',']','(',')','{', '}', '|'}
+
+    # create a stack to keep track of unmatched brackets.
     stack = []
+
+    # interate through the string character by character.
     for char in string:
+
+        # check if the char is a bracket.
+        # If not, continue to next character.
         if char not in possible_matches:
             continue
+
+        # If our stack is empty, we don't have anything to compare it to,
+        # so we know we have to add it to the stack and no other action is necessary.
         if len(stack) is 0:
             stack.append(char)
         else:
             # compare the new bracket to bracket at top of stack
+            # if the bracket at the top of the stack is an opening bracket AND
+            # char is the corresponding closing bracket, pop the bracket off the stack.
             if stack[len(stack) - 1] in bracket_dict and bracket_dict[stack[len(stack) - 1]] is char:
+
+                print('we are here') # This print command is an artifact of troubleshooting. Should be deleted.
+
                 # if match pop bracket off stack
-                print('we are here')
                 stack.pop()
+
             # if not match, add bracket to stack
             else:
                 stack.append(char)
+
+    # if there are still brackets left in the stack that means 
+    # there wasn't a matching closing bracket for each opening bracket and 
+    # we should return false.
+    # Otherwise, we return true.
     if len(stack) > 0:
         return False
     else:
